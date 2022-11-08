080afe07b8af9d1252b089c7b765f5c2718a1268
Juan Bonino <tomasboni9@gmail.com>
2022-11-08 19:19:19 -0300

    vercel

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import consumerInit from './kafka-consumer/index';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   consumerInit;
 }
 bootstrap();
